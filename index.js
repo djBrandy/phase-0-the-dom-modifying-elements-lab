8229abf34617b43fa7de0c1d871f0799cf008d56
djBrandy <dandobrandon0@gmail.com>
2024-04-01 08:57:35 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "YOUR-NAME is the champion"; 
+document.body.append(newHeader);
